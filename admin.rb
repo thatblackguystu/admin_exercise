c1776d44e566b03ba4d06e954528edfa8bea027c
jnappy <jpnappy@gmail.com>
2015-06-27 19:40:48 -0400

    first commit

---
+++ admin.rb
@@ -0,0 +1,30 @@
+require 'pry'
+
+#you run a site that offers a matching service between vendors selling laptops and customers 
+#..looking to purchase; you charge a fee to be listed as a vendor 
+#your site's users must sign in and if they are an admin they are able to make changes to the site
+
+#______________________________________________
+
+#write a function "user_permission" that accepts four parameters: signed_in, admin, paid, and canceled
+#if the user is signed in and they are an admin, show "you can see see and change all the pages!"
+#if you user is signed in but they are not an admin, show "you can see all the pages!"
+#if the user is not signed in, show "you can't see any of the pages, please sign in!"
+#if the user doesn't pay their bills or has canceled a deal, show "go away!"
+
+#ask the user if they are an admin (true/false)
+
+#store their answer in a variable
+
+#ask the user if they are signed in (true/false)
+
+#store their answer in a variable
+
+#ask the user if they pay their bills (true/false)
+
+#store their answer in a variable
+
+#ask the user if they have canceled a deal (true/false)
+
+#store their answer in a variable
+
