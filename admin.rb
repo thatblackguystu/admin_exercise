4aacc7c7495b6304f7d79a5badb4ea9fb01ff17d
jnappy <jpnappy@gmail.com>
2015-07-01 16:05:31 -0400

    changed the order of conditionals

--- admin.rb
+++ admin.rb
@@ -7,24 +7,26 @@ require 'pry'
 #______________________________________________
 
 #write a function "user_permission" that accepts four parameters: signed_in, admin, paid, and canceled
+#if the user doesn't pay their bills or has canceled a deal, show "go away!"
 #if the user is signed in and they are an admin, show "you can see see and change all the pages!"
 #if you user is signed in but they are not an admin, show "you can see all the pages!"
 #if the user is not signed in, show "you can't see any of the pages, please sign in!"
-#if the user doesn't pay their bills or has canceled a deal, show "go away!"
 
-#ask the user if they are an admin (true/false)
-
-#store their answer in a variable
-
-#ask the user if they are signed in (true/false)
+#ask the user if they pay their bills (yes/no)
 
 #store their answer in a variable
 
-#ask the user if they pay their bills (true/false)
+#ask the user if they have canceled a deal (yes/no)
 
 #store their answer in a variable
 
-#ask the user if they have canceled a deal (true/false)
+#ask the user if they are an admin (yes/no)
 
 #store their answer in a variable
+
+#ask the user if they are signed in (yes/no)
+
+#store their answer in a variable
+
+
 
